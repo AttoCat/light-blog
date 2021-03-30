76293c673e2a824a6f52dad9736dd95877aef8a7
AttoCat <wazasiyo2525a@gmail.com>
2021-03-30 18:39:17 +0900

    :hammer: Add signup(post-method)

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from lib import database
+
+print(generate_password_hash("Atto2525!"))
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def root() -> str:
+def index() -> str:
     return render_template("index.html")
 
 
 @app.route("/signup")
 def signup() -> str:
     return render_template("signup.html")
+
+
+@app.route("/signup", methods=["POST"])
+def post_signup() -> str:
+    print(request.form.get("password"))
+    print(request.form.get("username"))
+    # with database.cursor as cursor:
+    #     sql = "INSERT INTO light_blog.users (name,password) VALUES ($2,$3)"
+    #     cursor.execute(sql,)
+    return render_template("success-signup.html")
 
 
 app.run()
